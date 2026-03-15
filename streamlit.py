1833797e2c35178396771d2087037833464ea421
Kailash suthar <kailashsuthar0723@gmail.com>
2026-03-15 10:22:48 +0530

    Update model loading and slider labels in streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import pickle
+
 st.title("Iris Prediction")
 
-import pickle
-model = pickle.load(open("model_iris.pkl","rb"))
+with open("model_svm.pkl", "rb") as f:
+    model = pickle.load(f)
 
-sl = st.slider("Sl",2.0,10.0)
-sw = st.slider("Sw",2.0,10.0)
-pl = st.slider("pl",2.0,10.0)
-pw = st.slider("pw",2.0,10.0)
+sl = st.slider("Sepal Length", 2.0, 10.0)
+sw = st.slider("Sepal Width", 2.0, 10.0)
+pl = st.slider("Petal Length", 2.0, 10.0)
+pw = st.slider("Petal Width", 2.0, 10.0)
 
 if st.button("Predict"):
-    st.success(model.predict([[sl,sw,pl,pw]]))
+    prediction = model.predict([[sl, sw, pl, pw]])
+    st.success(prediction[0])
